8892671e1f9dfed16e62a7f2962907054dc99a2d
Lewis Cowles <lewiscowles@me.com>
2017-08-06 21:21:20 +0100

    Updated to use batch-calling
    updated to use batch-calling, uses a global (not ideal), initialises if not an array.
    Instead of SQL querying, adds data to an array for querying later `shutdown` hook (probably affects accuracy).

--- edits/wp-includes/class-wp-hook.php
+++ edits/wp-includes/class-wp-hook.php
@@ -1,6 +1,9 @@
 <?php
-
-	public function apply_filters( $value, $args ) {
+  public function apply_filters( $value, $args ) {
+		global $cd2_hook_log_sql_entries;
+		if(!is_array($cd2_hook_log_sql_entries)) {
+			$cd2_hook_log_sql_entries = [];
+		}
 		$log = [
 			'old'=>[
 				'value' => $value,
@@ -9,57 +12,46 @@
 			'runs' => []
 		]; $n = 0;
 
-		if ( ! $this->callbacks ) {
-			return $value;
-		}
+ 		if ( ! $this->callbacks ) {
+ 			return $value;
+ 		}
 
-		$nesting_level = $this->nesting_level++;
+ 		$nesting_level = $this->nesting_level++;
 
-		$this->iterations[ $nesting_level ] = array_keys( $this->callbacks );
-		$num_args = count( $args );
+ 		$this->iterations[ $nesting_level ] = array_keys( $this->callbacks );
+ 		$num_args = count( $args );
 
-		do {
-			$this->current_priority[ $nesting_level ] = $priority = current( $this->iterations[ $nesting_level ] );
+ 		do {
+ 			$this->current_priority[ $nesting_level ] = $priority = current( $this->iterations[ $nesting_level ] );
 
-			foreach ( $this->callbacks[ $priority ] as $the_ ) {
-				if( ! $this->doing_action ) {
-					$args[ 0 ] = $value;
-				}
+ 			foreach ( $this->callbacks[ $priority ] as $the_ ) {
+ 				if( ! $this->doing_action ) {
+ 					$args[ 0 ] = $value;
+ 				}
 
-				// Avoid the array_slice if possible.
-				if ( $the_['accepted_args'] == 0 ) {
-					$value = call_user_func_array( $the_['function'], array() );
-				} elseif ( $the_['accepted_args'] >= $num_args ) {
-					$value = call_user_func_array( $the_['function'], $args );
-				} else {
-					$value = call_user_func_array( $the_['function'], array_slice( $args, 0, (int)$the_['accepted_args'] ) );
-				}
-				$log["runs"][$n] = $value; $n++;
-			}
-		} while ( false !== next( $this->iterations[ $nesting_level ] ) );
+ 				// Avoid the array_slice if possible.
+ 				if ( $the_['accepted_args'] == 0 ) {
+ 					$value = call_user_func_array( $the_['function'], array() );
+ 				} elseif ( $the_['accepted_args'] >= $num_args ) {
+ 					$value = call_user_func_array( $the_['function'], $args );
+ 				} else {
+ 					$value = call_user_func_array( $the_['function'], array_slice( $args, 0, (int)$the_['accepted_args'] ) );
+ 				}
+ 				$log["runs"][$n] = $value; $n++;
+ 			}
+ 		} while ( false !== next( $this->iterations[ $nesting_level ] ) );
 
-		try {
-			$dbh = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST, DB_USER, DB_PASSWORD, [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
-			$stmt = $dbh->prepare('INSERT INTO new_table (tag, data, query, body, path, dt) VALUES (?, ?, ?, ?, ?, ?)');
+		$cd2_hook_log_sql_entries[] = $this->tag;
+		$cd2_hook_log_sql_entries[] = json_encode($log, $jsonOpts);
+		$cd2_hook_log_sql_entries[] = json_encode($_GET, $jsonOpts);
+		$cd2_hook_log_sql_entries[] = json_encode($_POST, $jsonOpts);
+		$cd2_hook_log_sql_entries[] = $_SERVER['REQUEST_URI'];
+		$cd2_hook_log_sql_entries[] = date('Y-m-d H:i:s');
 
-			$jsonOpts = (JSON_HEX_QUOT|JSON_HEX_AMP|JSON_HEX_APOS|JSON_HEX_TAG|JSON_PRETTY_PRINT);
+ 		unset( $this->iterations[ $nesting_level ] );
+ 		unset( $this->current_priority[ $nesting_level ] );
 
-			$stmt->bindParam(1, $this->tag);
-			$stmt->bindValue(2, json_encode($log, $jsonOpts));
-			$stmt->bindValue(3, json_encode($_GET, $jsonOpts));
-			$stmt->bindValue(4, json_encode($_POST, $jsonOpts));
-			$stmt->bindValue(5, $_SERVER['REQUEST_URI']);
-			$stmt->bindValue(6, date('Y-m-d H:i:s'));
+ 		$this->nesting_level--;
 
-			$stmt->execute();
-		} catch( Exception $e ) {
-			die($e->getMessage());
-		}
-
-		unset( $this->iterations[ $nesting_level ] );
-		unset( $this->current_priority[ $nesting_level ] );
-
-		$this->nesting_level--;
-
-		return $value;
-	}
+ 		return $value;
+ 	}

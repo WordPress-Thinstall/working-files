cb442b4fa45497724a05383b11964ee84c8b5bd9
Lewis Cowles <lewiscowles@me.com>
2017-08-06 22:08:27 +0100

    added activation hook to create table

--- plugins/wp-save-hooks-sql.php
+++ plugins/wp-save-hooks-sql.php
@@ -7,6 +7,25 @@ Version: 1.00
 Author URI: https://www.codesign2.co.uk/
 */
 
+register_activation_hook(__FILE__, function() {
+	$sql = 'CREATE TABLE IF NOT EXISTS `wp_hook_log` (
+	  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+	  `tag` varchar(255) NOT NULL,
+	  `data` text NOT NULL,
+	  `dt` datetime NOT NULL,
+	  `query` text NOT NULL,
+	  `body` text NOT NULL,
+	  `path` varchar(255) NOT NULL,
+	  PRIMARY KEY (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8';
+  try {
+    $dbh = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST, DB_USER, DB_PASSWORD, [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+    $dbh->query($sql);
+  } catch( Exception $e ) {
+    die($e->getMessage());
+	}
+});
+
 add_action('shutdown', function() {
   global $cd2_hook_log_sql_entries;
   if(!is_array($cd2_hook_log_sql_entries)) {
